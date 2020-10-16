c48293cecb6682a66676023bd38491acc404592b
Rowsej <42834129+Rowsej@users.noreply.github.com>
2020-10-16 20:17:57 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function selectEl(query) {
+	return document.querySelector(query);
+}
+function selectEls(query) {
+	return document.querySelectorAll(query);
+}
+function updateState(e) {
+	var state = e.state;
+	alert(state);
+}
+function changeState(id, page) {
+	history.pushState({ pageId: id }, "Page | Rowsej's Website", page);
+}
+window.onload = function() {
+	selectEls("a button").forEach(btn => {
+		btn.onclick = function(e) {
+			alert(e.target.innerHTML);
+		}
+	});
+	window.onpopstate = updateState;
+};
