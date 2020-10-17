21169e13009296e2629ef0675012c4855e879dfb
Rowsej <42834129+Rowsej@users.noreply.github.com>
2020-10-17 13:07:23 +1100

    Update script.js
    Arghh why is this not working?

--- script.js
+++ script.js
@@ -11,12 +11,14 @@ function updateState(e) {
 }
 function changeState(id, page) {
 	history.pushState({ pageId: id }, "Page | Rowsej's Website", page);
+	document.title = "Page | Rowsej's Website";
+	alert("Changed state!");
 }
-window.onload = function() {
+window.onload = () => {
 	selectEls("a button").forEach(btn => {
 		btn.onclick = function(e) {
-			alert("Clicked: " + e.target.innerHTML);
 			var page = btn.parentNode.href;
+			alert("Clicked: " + page);
 			changeState(page.replace(".html", ""), page);
 			e.preventDefault();
 		};
